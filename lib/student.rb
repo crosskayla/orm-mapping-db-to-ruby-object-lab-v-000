2ac10106d272099a1d32178b0b218d1bf5d94b16
Kayla Cross <kac288@georgetown.edu>
2018-06-20 18:05:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -123,7 +123,9 @@ class Student
     WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end.first
     
   end
 
